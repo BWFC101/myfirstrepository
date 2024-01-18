c2bcb70de20ec9a5f48131b1fbfdbc88424b1ea5
BWFC101 <156912890+BWFC101@users.noreply.github.com>
2024-01-18 06:18:02 -0500

    Add files via upload
    main.py.

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    start_year = int(input("Enter the start year: "))
+    end_year = int(input("Enter the end year: "))
+    age = end_year - start_year
+    print("Calculated age:", age)
+
+main()
