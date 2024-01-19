2a7e2b9cb79dc202d1f06d886e587c0cc3af60e4
BWFC101 <156912890+BWFC101@users.noreply.github.com>
2024-01-19 01:46:29 -0500

    Add files via upload
    Mini-Project Calculator

---
+++ main.py.2.py
@@ -0,0 +1,11 @@
+first_number = int(input('Enter your first number: '))
+second_number = int(input('Enter your second number: '))
+
+print('{} + {} = '.format(first_number, second_number))
+print(first_number + second_number)
+print('{} - {} = '.format(first_number, second_number))
+print(first_number - second_number)
+print('{} * {} = '.format(first_number, second_number))
+print(first_number * second_number)
+print('{} / {} = '.format(first_number, second_number))
+print(first_number / second_number)
