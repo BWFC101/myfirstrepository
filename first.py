d04ba4d9271b06b5525a15768bbf6a0338ad5223
BWFC101 <156912890+BWFC101@users.noreply.github.com>
2024-01-19 03:21:18 -0500

    Add files via upload
    Introduction To Me

---
+++ first.py
@@ -0,0 +1,12 @@
+def print_introduction():
+    name = "Daniel"
+    age = "36"  
+    project_summary = " what to learn code using python."
+
+    print("Introduction:")
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print("Project Summary:")
+    print(project_summary)
+
+print_introduction()
